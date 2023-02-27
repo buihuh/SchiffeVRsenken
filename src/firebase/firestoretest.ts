1630f71fc228a1e4668ef12c18315433b6412e5b
Vero <veronikaseidel95@web.de>
2023-02-27 17:54:46 +0100

    add match data to firebase

---
+++ src/firebase/firestoretest.ts
@@ -0,0 +1,34 @@
+import * as FB from "./init.js";
+import {Player} from "../game/player.js";
+import {getStartedField, Match, Players} from "../game/match.js";
+
+/**
+ * TODO start create game test
+ */
+
+const player1 = new Player(1, "Max", 0, true);
+console.log(player1);
+
+const player2 = new Player(2, "Moritz", 0, false);
+console.log(player2);
+
+const field1 = getStartedField();
+const field2 = getStartedField();
+
+field1[0][0].hasShip = true;
+field1[0][1].hasShip = true;
+field2[0][0].hasShip = true;
+field2[1][0].hasShip = true;
+
+const match = new Match(player1, player2, Players.Player1, 0, field1, field2);
+
+const firebase = new FB.Firebase();
+firebase.createGame(match)
+    .then(res => {
+        console.log('match created')
+    }).catch(err => {
+        console.log('something went wrong '+ err)
+    });
+/**
+ * TODO end create game test
+ */
--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -2,7 +2,13 @@
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
 // @ts-ignore
 import {getAnalytics} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js';
+// @ts-ignore
+import {getFirestore, doc, addDoc, serverTimestamp, collection, updateDoc, setDoc} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
 import {firebaseConfig} from './firebase-config.js';
+import * as MATCH from './../game/match.js';
+import {Field} from "../game/field.js";
+import {Player} from "../game/player.js";
+
 
 // Somehow this is not working:
 // import {initializeApp} from "firebase/app";
@@ -14,7 +20,120 @@ import {firebaseConfig} from './firebase-config.js';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-console.log(analytics)
+export class Firebase {
+
+    protected app : any;
+    protected analytics: any;
+    protected db : any;
+    gameid : number;
+
+    constructor() {
+        // Initialize Firebase
+        this.app = initializeApp(firebaseConfig);
+        this.analytics = getAnalytics(this.app);
+        this.db = getFirestore(this.app);
+    }
+
+    async createGame(match: MATCH.Match) {
+        const docRef = await addDoc(collection(this.db, 'games'), {
+            created: serverTimestamp(),
+            state: 0,
+            player1: {id: match.player1.id, name:match.player1.name},
+            player2: {id: match.player2.id, name:match.player2.name},
+            attacker: match.attacker
+        });
+        this.gameid = docRef.id;
+
+        await this.createField(match.fieldPlayer1, 1);
+        await this.createField(match.fieldPlayer2, 2);
+
+        // return docRef;
+    }
+
+
+    async createField(field: Field [][], player: number) {
+
+        for (let y: number = 0; y < field.length; y++) {
+            for (let x: number = 0; x < field.length; x++) {
+
+                const c = doc(this.db,
+                    'games',
+                    this.gameid,
+                    'fieldPlayer' + player,
+                    'x'+x+'y'+y)
+                    .withConverter(fieldConverter);
+                await setDoc(c, new Field(
+                        field[x][y].positionX,
+                        field[x][y].positionY,
+                        field[x][y].hasShip
+                    )
+                );
+            // .then(res => {
+            //         console.log('created playerfield ' +player)
+            //     }).catch(err => {
+            //         console.log('something went wrong '+ err)
+            //     })
+
+            }
+        }
+    }
+
+    updateMatch(match: MATCH.Match) {
+        const docRef = doc(this.db, 'games', this.gameid);
+        updateDoc(docRef, {
+        });
+
+    }
+
+
+}
+
+/*
+* TODO fix data types
+* */
+const matchConverter = {
+    toFirestore: (match) => {
+        return {
+            player1: match.player1.id,
+            player2: match.player2.id,
+            fieldPlayer1: match.fieldPlayer1,
+            fieldPlayer2: match.fieldPlayer2,
+            attacker: match.attacker
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new MATCH.Match(data.player1, data.player2, data.attacker, data.fieldPlayer1, data.fieldPlayer2);
+    }
+};
+
+
+const fieldConverter = {
+    toFirestore: (field) => {
+        return {
+            x: field.positionX,
+            y: field.positionY,
+            hasShip: field.hasShip,
+            isHit: field.isHit
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new Field(data.positionX, data.positionY, data.hasShip);
+    }
+};
+
+const playerConverter = {
+    toFirestore: (player) => {
+        return {
+            id: player.id,
+            name: player.name,
+            winCounter: player.winCounter,
+            isHost: player.isHost
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new Player(data.id, data.name);
+    }
+};
