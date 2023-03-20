129f03bce5d8063e166ab4670a2d2756feaf6c55
Siu Cheng <herrcheng@gmail.com>
2023-03-21 00:11:56 +0100

    :zap: add right controller sync

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -42,10 +42,15 @@ export class Firebase {
             player1Rotation: [0.0, 0.0, 0.0],
             player1ControllerLeftPosition: [0.0, 0.0, 0.0],
             player1ControllerLeftRotation: [0.0, 0.0, 0.0],
+            player1ControllerRightPosition: [0.0, 0.0, 0.0],
+            player1ControllerRightRotation: [0.0, 0.0, 0.0],
+
             player2Position: [0.0, 0.0, 0.0],
             player2Rotation: [0.0, 0.0, 0.0],
             player2ControllerLeftPosition: [0.0, 0.0, 0.0],
-            player2ControllerLeftRotation: [0.0, 0.0, 0.0]
+            player2ControllerLeftRotation: [0.0, 0.0, 0.0],
+            player2ControllerRightPosition: [0.0, 0.0, 0.0],
+            player2ControllerRightRotation: [0.0, 0.0, 0.0],
             // state: 0,
             // player1: {id: match.player1.id, name: match.player1.name, winCounter: 0, isHost: true} as Player,
             // player2: {id: match.player2.id, name: match.player2.name, winCounter: 0, isHost: false} as Player,
