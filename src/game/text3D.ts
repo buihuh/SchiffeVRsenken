80adc3d59d13cb857ae8398b091d440cf1ebcf9d
Siu Cheng <herrcheng@gmail.com>
2023-03-22 17:52:00 +0100

    :construction: change color when focus

--- src/game/text3D.ts
+++ src/game/text3D.ts
@@ -1,6 +1,20 @@
 import {GameObject} from './gameobjects.js';
+import * as THREE from 'three';
 
 export class Text3D extends GameObject {
     text: string;
     started:boolean = false;
+
+    onFocus() {
+        const material = this.mesh.material as THREE.MeshPhongMaterial;
+        material.color = new THREE.Color(255, 0, 0);
+        material.specular = new THREE.Color(255, 0, 0);
+    }
+
+
+    onUnfocus() {
+        const material = this.mesh.material as THREE.MeshPhongMaterial;
+        material.color = new THREE.Color(255, 255, 0);
+        material.specular = new THREE.Color(255, 255, 0);
+    }
 }
