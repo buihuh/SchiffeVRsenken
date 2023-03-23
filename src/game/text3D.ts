9db7a7bfa26d4fdc736d202169081d37dc9e528f
Siu Cheng <herrcheng@gmail.com>
2023-03-23 15:33:09 +0100

    :pushpin: wip add 3 text objects

--- src/game/text3D.ts
+++ src/game/text3D.ts
@@ -1,40 +1,97 @@
 import {GameObject} from './gameobjects.js';
 import * as THREE from 'three';
-import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js';
 import {Vector3} from 'three';
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js';
 
 export class Text3D extends GameObject {
     text: string;
     started:boolean = false;
     font;
     counter = 0;
+    material;
+    callbackFunction = null;
 
-    onFocus() {
-        if (!this.started) {
-            const material = this.mesh.material as THREE.MeshPhongMaterial;
-            material.color = new THREE.Color(219, 255, 235);
-            material.specular = new THREE.Color(219, 255, 235);
+    constructor(position, scene, meshList, objectList, // for super
+                text: string, font, rotation: THREE.Vector3 = null, callbackFunction = null // for this class
+    ) {
+        super(new TextGeometry(text, {
+            font: font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 10,
+            bevelEnabled: true,
+            bevelThickness: 0.02,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 5
+        }), new THREE.MeshPhongMaterial({
+            color: new THREE.Color(219, 255, 235),
+            specular: new THREE.Color(219, 255, 235)
+        }), position, scene, meshList, objectList, rotation);
+        this.text = text;
+        this.font = font;
+        this.mesh.geometry.center();
+        this.mesh.geometry.computeBoundingBox();
+        this.meshList.push(this.mesh);
+        this.callbackFunction = callbackFunction;
+    }
 
-            this.mesh.geometry = new TextGeometry("SchiffeVRsenken " + this.counter++, {
-                font: this.font, size: 1, height: 0.2, curveSegments: 10, bevelEnabled: true,
-                bevelThickness: 0.02,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-            });
-            this.mesh.geometry.center();
-            this.mesh.geometry.computeBoundingBox();
-
-            this.started = true;
+    onSelectStart() {
+        if (this.callbackFunction) {
+            this.callbackFunction();
         }
     }
 
+    onFocus() {
+    }
 
     onUnfocus() {
+    }
+
+    /**
+     * Start Setter
+     */
+
+    setText(newText: string) {
+        this.text = newText;
+        this.mesh.geometry.dispose();
+        this.mesh.geometry = this.getGeometry();
+    }
+
+    setCallback(newCallbackFunction) {
+        this.callbackFunction = newCallbackFunction;
+    }
+
+    /**
+     * End Setter
+     */
+
+    /**
+     * Start Helper
+     */
+
+    getMaterial() {
         const material = this.mesh.material as THREE.MeshPhongMaterial;
         material.color = new THREE.Color(219, 255, 235);
-        material.specular = new THREE.Color(219, 228, 235);
-        this.started = false;
+        material.specular = new THREE.Color(219, 255, 235);
+        return material;
+    }
+
+    getGeometry() {
+        const geometry = new TextGeometry(this.text, {
+            font: this.font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 10,
+            bevelEnabled: true,
+            bevelThickness: 0.02,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+        geometry.center();
+        geometry.computeBoundingBox();
+        return geometry;
     }
 
 
@@ -44,4 +101,8 @@ export class Text3D extends GameObject {
         boundingBox.getSize(size);
         return size;
     }
+
+    /**
+     * End Helper
+     */
 }
